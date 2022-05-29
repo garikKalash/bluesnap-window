e143a036fd7bb26038baae65c30910ebba8184ed
Garik <garik@jazva.com>
2022-05-30 01:17:05 +0400

    fix configs

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/bluesnap-payment'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 4200);
+
 const path = require('path');
 
-const app = express();
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-app.use(express.static(__dirname + '/dist'));
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/bluesnap-payment/index.html'));
 });
-
-app.listen(process.env.PORT || 4200);
