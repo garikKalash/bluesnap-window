be1e890be66714f46c913ffde16c96ae67257bea
Garik <garik.kalash@gmail.com>
2023-06-28 00:27:00 +0400

    fix

---
+++ src/app/components/update-plan/update-plan.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {PaymentService} from "../../service/payment.service";
+import {PaymentPlan} from "../../models/payment-plan.model";
+
+@Component({
+  selector: 'app-update-plan',
+  templateUrl: './update-plan.component.html',
+  styleUrls: ['./update-plan.component.css']
+})
+export class UpdatePlanComponent implements OnInit {
+
+  plan_id = new URLSearchParams(window.location.search).get('planId')
+  uid = new URLSearchParams(window.location.search).get('uid')
+
+  planDetail: PaymentPlan | undefined;
+  constructor(private paymentService: PaymentService) { }
+
+  ngOnInit(): void {
+    if(this.plan_id && this.uid){
+      this.paymentService.getPlanDetails(this.plan_id, this.uid)
+        .subscribe(res=>{
+          this.planDetail = res
+        });
+    }
+  }
+
+}
