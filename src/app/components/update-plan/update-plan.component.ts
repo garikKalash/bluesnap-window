69ef58ffa523d158c428a6ea7ed534c8f645b013
Kruti-VN <kruti@treatva.com>
2025-01-23 18:34:42 +0200

    support for server-version url

--- src/app/components/update-plan/update-plan.component.ts
+++ src/app/components/update-plan/update-plan.component.ts
@@ -11,13 +11,14 @@ export class UpdatePlanComponent implements OnInit {
 
   plan_id = new URLSearchParams(window.location.search).get('planId')
   uid = new URLSearchParams(window.location.search).get('uid')
+  server_version = new URLSearchParams(window.location.search).get('server-version')
 
   planDetail: PaymentPlan | undefined;
   constructor(private paymentService: PaymentService) { }
 
   ngOnInit(): void {
     if(this.plan_id && this.uid){
-      this.paymentService.getPlanDetails(this.plan_id, this.uid)
+      this.paymentService.getPlanDetails(this.plan_id, this.uid, this.server_version)
         .subscribe(res=>{
           this.planDetail = res
         });
